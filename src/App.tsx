c87560e2e4a89a4544b83e91adbb18045050afc0
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2026-03-03 14:11:16 +0300

    typo_corrected

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from "./pages/sections/Hero";
 import { Projects } from "./pages/sections/Projects";
 import { Experience } from "./pages/sections/Experiences";
 import { Technologies } from "./pages/sections/Technologies";
-import { Contact } from "./pages/sections/contacts";
+import { Contact } from "./pages/sections/Contacts";
 import { Footer } from "./pages/Footer";
 
 export default function App() {
